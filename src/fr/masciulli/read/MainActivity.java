9fc7c5b4c49ff05c4f2dd30400ed69aeb141a870
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-06-23 22:54:54 +0200

    Add options menu for settings and about

--- src/fr/masciulli/read/MainActivity.java
+++ src/fr/masciulli/read/MainActivity.java
@@ -2,6 +2,7 @@ package fr.masciulli.read;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends Activity {
     /**
@@ -11,5 +12,11 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
