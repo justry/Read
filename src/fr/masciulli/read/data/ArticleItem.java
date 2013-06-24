4edfad848f62b4ecf32d628fb1b0880569b49dc7
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-06-24 19:01:46 +0200

    Add more things in an Article

--- src/fr/masciulli/read/data/ArticleItem.java
+++ src/fr/masciulli/read/data/ArticleItem.java
@@ -1,10 +1,19 @@
 package fr.masciulli.read.data;
 
+import org.joda.time.DateTime;
+
 public class ArticleItem {
     private String mTitle;
+    private String mAuthor;
+    private DateTime mDate;
+    private String content;
 
-    public ArticleItem(String title) {
+    public ArticleItem(String title, String author, DateTime date, String content) {
         mTitle = title;
+    }
+
+    public ArticleItem() {
+
     }
 
     public String getTitle() {
@@ -13,5 +22,29 @@ public class ArticleItem {
 
     public void setTitle(String title) {
         this.mTitle = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public void setAuthor(String author) {
+        this.mAuthor = author;
+    }
+
+    public DateTime getDate() {
+        return mDate;
+    }
+
+    public void setDate(DateTime date) {
+        this.mDate = date;
     }
 }
--- src/fr/masciulli/read/data/ArticleItemProvider.java
+++ src/fr/masciulli/read/data/ArticleItemProvider.java
@@ -1,24 +1,19 @@
 package fr.masciulli.read.data;
 
+import org.joda.time.DateTime;
+
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class ArticleItemProvider {
-    private static List<ArticleItem> mArticleItems = new ArrayList<ArticleItem>(Arrays.asList(new ArticleItem[]{
-            new ArticleItem("Title of my article"),
-            new ArticleItem("Title of my article"),
-            new ArticleItem("Title of my article"),
-            new ArticleItem("Title of my article"),
-            new ArticleItem("Title of my article"),
-            new ArticleItem("Title of my article"),
-            new ArticleItem("Title of my article"),
-            new ArticleItem("Title of my article"),
-            new ArticleItem("Title of my article")
-    }));
 
     public static List<ArticleItem> getItems() {
-        return mArticleItems;
+        List<ArticleItem> articleItems = new ArrayList<ArticleItem>();
+
+        for (int i = 0; i < 15; i++) {
+            articleItems.add(new ArticleItem("Article title", "Alexandre Masciulli", new DateTime(), "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non sine causa ex iis memoriae ducta sit disciplina. Quare hoc videndum est, possitne nobis hoc ratio philosophorum dare. Equidem, sed audistine modo de Carneade?\n\nDuo Reges: constructio interrete. Venit ad extremum; Huius, Lyco, oratione locuples, rebus ipsis ielunior. Id mihi magnum videtur. Si quicquam extra virtutem habeatur in bonis. Scientiam pollicentur, quam non erat mirum sapientiae cupido patria esse cariorem. Respondent extrema primis, media utrisque, omnia omnibus. Multa sunt dicta ab antiquis de contemnendis ac despiciendis rebus humanis"));
+        }
+
+        return articleItems;
     }
 }
