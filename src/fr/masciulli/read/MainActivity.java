fe2bfd5a9b1cafd743fbdf40abfb37b8f3af7f99
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2013-06-24 12:06:58 +0200

    Add todo

--- src/fr/masciulli/read/MainActivity.java
+++ src/fr/masciulli/read/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends Activity implements FeedListFragment.Callbacks
                 break;
 
             case R.id.settings:
-
+                // TODO implement settings Activity
                 break;
         }
         return true;
