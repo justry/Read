49a83987c388a945e3c875c3c6bb4e29fe6bfda6
Thomas <thomas.keunebroek@gmail.com>
2013-06-26 14:42:41 +0200

    Add a controller fragment layer to decouple code

---
+++ src/fr/masciulli/read/fragment/ControllerFragment.java
@@ -0,0 +1,53 @@
+package fr.masciulli.read.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.squareup.otto.Subscribe;
+
+import fr.masciulli.read.BusProvider;
+import fr.masciulli.read.OpenArticleEvent;
+import fr.masciulli.read.R;
+
+/**
+ * A UI-less Fragment that acts like an abstract controller layer for Otto bus events.
+ */
+public class ControllerFragment extends Fragment {
+	public static final String FRAGMENT_TAG = "Controller";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setRetainInstance(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return null;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		BusProvider.getInstance().register(this);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		BusProvider.getInstance().unregister(this);
+	}
+
+	@Subscribe
+	@SuppressWarnings("unused")
+	public void onOpenArticle(OpenArticleEvent event) {
+		getFragmentManager().beginTransaction()
+				.replace(R.id.articlelistfragmentcontainer, ArticleDetailFragment.newInstance(event.item))
+				.commit();
+
+	}
+}
